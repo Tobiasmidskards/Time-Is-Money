c8c75503b638792512a11e522cce88750672bc6c
Tobiasmidskards <fqn295@alumni.ku.dk>
2017-01-09 20:38:11 +0100

    Initial Commit

---
+++ TimeIsMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeIsMoney
+//
+//  Created by Tobias Midskard on 05/01/2017.
+//  Copyright © 2017 Tobias Midskard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
